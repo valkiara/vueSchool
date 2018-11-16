0370663894d16f14362fcb7fb5b22225699917f2
Beka Latsabidze <blatsabidze@outlook.com>
2018-11-16 13:31:01 +0400

    app structure done

--- vueschool-forum/src/components/HelloWorld.vue
+++ vueschool-forum/src/components/HelloWorld.vue
@@ -1,22 +1,49 @@
 <template>
   <div>
-    <h1>Welcome to the Forum</h1>
+    <div v-for="thread in threads" v-bind:key="thread.key" class="col-large push-top">
 
-    <div v-for="thread in threads" v-bind:key="thread.key">
-      <h2>{{thread.title}}</h2>
+        <h1>{{thread.title}}</h1>
 
-      <div v-for="postId in thread.posts" v-bind:key="postId.key">
-        <p>{{users[posts[postId].userId].name}}</p>
-        <p>{{posts[postId].text}}</p>
-      </div>
+        <div  v-for="postId in thread.posts" v-bind:key="postId.key"  class="post-list">
 
+            <div  class="post">
+
+                <div class="user-info">
+                    <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+                    
+
+                    <a href="#">
+                        <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
+                    </a>
+
+                    <p class="desktop-only text-small">107 posts</p>
+
+
+
+                </div>
+
+                <div class="post-content">
+                    <div>
+                      <p>
+                        {{posts[postId].text}}
+                      </p>
+                    </div>
+                </div>
+
+
+
+                <div class="post-date text-faded">
+                    {{posts[postId].publishedAt}}
+                </div>
+            </div>
+        </div>
+
+        
     </div>
-
   </div>
 </template>
 
 <script>
-
 import sourceData from '@/data'
 console.log(sourceData)
 export default {
