37810770cdc63bff0e869299e54db8f46c9a270c
Beka Latsabidze <blatsabidze@outlook.com>
2018-11-18 18:56:13 +0400

    13 not found page

---
+++ vueschool-forum/src/components/pages/PageNotFound.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="col-full">
+    <h1>Not Found</h1>
+    <p>ნახუი 
+        <router-link :to="{name: 'Home'}">
+            ჰოუმ პეიჯზე
+        </router-link>
+    </p>
+</div>
+    
+</template>
+<script>
+    
+</script>
+<style scoped>
+h1{
+  font-size: 100px;
+}
+p{
+  font-size: 50px;
+  font-weight: 100;
+}
+div{
+  text-align: center;
+}
+
+</style>
