8955adede99db8af37e044079a2a31639a5f735d
Beka Latsabidze <blatsabidze@outlook.com>
2018-11-18 19:56:34 +0400

    15 postlist postlistitem comp.

---
+++ vueschool-forum/src/components/PostListItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="post">
+    <div  class="post">
+      <div class="user-info">
+        <a href="#" class="user-name">{{user.name}}</a>
+        <a href="#">
+        <img class="avatar-large" :src="user.avatar" alt="">
+        </a>
+        <p class="desktop-only text-small">{{UserPostCount}} posts</p>
+        </div>
+        <div class="post-content">
+          <div>
+            <p>
+            {{post.text}}
+            </p>
+          </div>
+        </div>
+        <div class="post-date text-faded">
+        {{post.publishedAt}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+export default {
+  props: {
+    post: {
+      require: true,
+      type: Object
+    }
+  },
+
+  computed: {
+    user () {
+      return sourceData.users[this.post.userId]
+    },
+
+    UserPostCount () {
+      return Object.keys(this.user.posts).length
+    }
+  }
+}
+</script>
--- vueschool-forum/src/components/pages/PageThreadShow.vue
+++ vueschool-forum/src/components/pages/PageThreadShow.vue
@@ -1,49 +1,21 @@
 <template>
     <div>
         <div class="col-large push-top">
-
             <h1>{{thread.title}}</h1>
-
-            <div  v-for="postId in thread.posts" v-bind:key="postId.key"  class="post-list">
-
-                <div  class="post">
-
-                    <div class="user-info">
-                        <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
-                        
-
-                        <a href="#">
-                            <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-                        </a>
-
-                        <p class="desktop-only text-small">107 posts</p>
-
-
-
-                    </div>
-
-                    <div class="post-content">
-                        <div>
-                        <p>
-                            {{posts[postId].text}}
-                        </p>
-                        </div>
-                    </div>
-
-
-
-                    <div class="post-date text-faded">
-                        {{posts[postId].publishedAt}}
-                    </div>
-                </div>
-            </div>
+            <PostList :posts="posts"/>
         </div>
   </div>
 </template>
 <script>
 import sourceData from '@/data'
+import PostList from '@/components/PostList'
 console.log(sourceData)
+
 export default {
+  components: {
+    PostList
+  },
+
   props: {
     id: {
       required: true,
@@ -57,6 +29,14 @@ export default {
       posts: sourceData.posts,
       users: sourceData.users
     }
+  },
+
+  computed: {
+    psots () {
+      const postIds = Object.keys(this.thread.posts)
+      return Object.values(sourceData.posts).filter(post => postIds.includes(post['.key']))
+    }
+
   }
 }
 </script>
